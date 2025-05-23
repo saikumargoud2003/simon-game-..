24929ed7c8e66bb1855011bbe821002d0dd41de7
saikumargoud2003 <patangisaikumargoud@gmail.com>
2025-05-23 11:24:53 +0530

    Fixed Simon game logic and restart bug

--- app.js
+++ app.js
@@ -1,62 +1,73 @@
-let gamseq=[];
-let userseq=[];
-let btns =["red","yellow","green","purpel"];
-  let started = false;
-  let level =0;
+let gamseq = [];
+let userseq = [];
+let btns = ["red", "yellow", "green", "purple"];
+let started = false;
+let level = 0;
 
-  let h2 = document.querySelector("h2");
+let h2 = document.querySelector("h2");
 
-  document.addEventListener("keypress", function (){
-    if( started == false){
-        console.log("game is started");
-        started = true;
-        levup();
-    }
-  });
-  function btnflash(btn){
-    btn.classList.add("flash");
-    setTimeout(function (){
-        btn.classList.remove("flash");
-    },250);
-
+document.addEventListener("keypress", function () {
+  if (!started) {
+    console.log("Game is started");
+    started = true;
+    level = 0;
+    gamseq = [];
+    levup();
   }
+});
 
-  function levup(){
-    userseq = [];
-    level++;
-    h2.innerText = `level ${level}`;
-    
-    let rdn = Math.floor(Math.random()*3);
-    let rdncolor = btns[rdn];
-    let rndbut = document.querySelector(`.${rdncolor}`);
-    gamseq.push(rdncolor);
+function btnflash(btn) {
+  btn.classList.add("flash");
+  setTimeout(function () {
+    btn.classList.remove("flash");
+  }, 250);
+}
 
-    btnflash(rndbut);
+function levup() {
+  userseq = [];
+  level++;
+  h2.innerText = `Level ${level}`;
 
+  let rdn = Math.floor(Math.random() * 4); // Changed from 3 to 4
+  let rdncolor = btns[rdn];
+  let rndbut = document.querySelector(`.${rdncolor}`);
+  gamseq.push(rdncolor);
+
+  btnflash(rndbut);
+}
+
+function checkAns(indx) {
+  if (gamseq[indx] === userseq[indx]) {
+    if (userseq.length === gamseq.length) {
+      setTimeout(levup, 500); // Wait a bit before next level
+    }
+  } else {
+    h2.innerText = `Game Over! Press any key to restart.`;
+    document.body.classList.add("game-over");
+    setTimeout(() => {
+      document.body.classList.remove("game-over");
+    }, 200);
+
+    resetGame();
   }
-   function checkAns(indx){
-    
-    if(gamseq[indx]==userseq[indx]){
-      if(userseq.length == userseq.length){
-        levup();
-      }
-    }
-    else{
-      h2.innerText =`Game over...! plz press any key to restart `
-    }
-   }
-  function btnpress(){
-    console.log("btnpressed");
-    let bttn = this;
-    btnflash(bttn);
-    let usercol = bttn.getAttribute("id");
-    userseq.push(usercol);
-    console.log(usercol);
-    checkAns(userseq.length-1);
+}
 
-  }
-   let allbtns = document.querySelectorAll(".btn") 
-    for(btnss of allbtns){
-        btnss.addEventListener("click",btnpress)
-    }
-   
+function btnpress() {
+  let bttn = this;
+  btnflash(bttn);
+  let usercol = bttn.getAttribute("id");
+  userseq.push(usercol);
+  checkAns(userseq.length - 1);
+}
+
+function resetGame() {
+  started = false;
+  gamseq = [];
+  userseq = [];
+  level = 0;
+}
+
+let allbtns = document.querySelectorAll(".btn");
+for (let btnss of allbtns) {
+  btnss.addEventListener("click", btnpress);
+}
